05b388c5027ef0239ff23072c630e2e7402b2f28
bina1886 <austinbina18@gmail.com>
2022-12-09 18:53:20 -0800

    bina

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more header gets there */
+
+/* betty style doc for function main gets there */
+/**
+ * main - Entry - point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
