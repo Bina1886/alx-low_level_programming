1a62fe5a844447f6c1a5ebaa7f06e186025c2f3a
bina1886 <austinbina18@gmail.com>
2022-12-09 18:07:00 -0800

    bina

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,26 @@
-yyyyuuiiiiiiii
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more header gets there */
+
+/* betty style doc for function main gets there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
