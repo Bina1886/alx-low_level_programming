86b3f878162ec8b33b9ec3475062cc1f1fa9e363
bina1886 <austinbina18@gmail.com>
2022-12-09 20:41:21 -0800

    bina

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,37 @@
-ii
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more header gets there */
+
+/* betty style doc for function main gets there */
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int d, p, q;
+
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
+		{
+			for (q = p + 1; q <= '9'; q++)
+			{
+				if ((p != d) != q)
+				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
+					if (d == '7' && p == 'g')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
