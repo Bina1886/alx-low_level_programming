e4cf110ed1fd66474b7ac28a7cc7dc5b2e4a0c35
bina1886 <austinbina18@gmail.com>
2022-12-09 18:17:32 -0800

    bina

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more header gets there */
+
+/* betty style doc for function main gets there */
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
