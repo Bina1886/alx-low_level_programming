d3d57008b04883493407769f27c27189a77c8660
bina1886 <austinbina18@gmail.com>
2022-12-14 06:16:52 -0800

    first task

---
+++ 0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - it test function that prints integer is positive or nagative
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_nagative(i);
+
+	return (0);
+}
