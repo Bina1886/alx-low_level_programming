e2503e985564fc29de5a72498bca83ec676b58f2
bina1886 <austinbina18@gmail.com>
2023-01-02 07:57:12 -0800

    bina

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,7 +9,7 @@
  *
  *  Return: pointer to memory area s.
  */
-char *_memset(char *s, char b, unsigned int n)
+void *_memset(void *s, int c, size_t n) 
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
