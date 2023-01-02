b46a248edb71cb513c1845e020e613fc5b8c444c
bina1886 <austinbina18@gmail.com>
2023-01-02 07:39:38 -0800

    bina

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,20 +4,17 @@
  * fills the first n bytes of the memory area pointed
  *  with the constant b.
  *  @b: constant to fill memory with.
- *  @s: pointer to memory area.
+ *  @s: pointer to memory areai.
  *  @n: bytes of the memory area to be filled.
  *
  *  Return: pointer to memory area s.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int counter;
+	unsigned int index;
+	unsigned char *memory = s, value = c;
 
-	for (counter = 0; counter < n; counter++)
-	{
-		s[counter] = b;
-	}
-	return (s);
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+	return (memory);
 }
-
