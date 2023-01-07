3c392cb175b7c96a2824b5afcf6d6ff37a64a620
Bina1886 <101872172+Bina1886@users.noreply.github.com>
2023-01-06 18:58:50 -0800

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,19 +5,12 @@
  * Return: void
  */
 void _puts_recursion(char *s)
-	/* It works by first checking if the current character, 
-	pointed to by s, is the null character ('\0'), which
-	indicates the end of the string. If it is, the
-	function calls _putchar to output a newline 
-	character and then returns. */
 {
 	if (*s == '\0')
-		/* according to instrusction it should print a new line */
 	{
 		_putchar('\n');
 		return;
 	}
-	/* if its not a null char then it should call itself with s + 1 */
 	_putchar (*s);
 	_puts_recursion(s + 1);
 }
